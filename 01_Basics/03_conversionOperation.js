1ce5f760c64c47c721777b7d09e3485c4f4680f4
Khawaja Hassan Shahid <mrbeastvenom69@gmail.com>
2024-11-28 19:21:45 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 50
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+//"33" => 33
+// "33abc" =>NaN
+// true => 1; false=> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hassan" => true
