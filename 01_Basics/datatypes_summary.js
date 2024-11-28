7e55a52478e476bd2eaf92ee4a391e753fa960bd
Khawaja Hassan Shahid <mrbeastvenom69@gmail.com>
2024-11-28 20:04:07 +0000

    Datatypes Summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive Datatypes
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol,, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+// Reference Type (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Spiderman", "Iron-Man", "Thor", "Hulk", "Daredevil"];
+let myObj = {
+    name: "Hassan",
+    age: 22,
+}
+
+const myFunction = function()
+{
+    console.log("Hello World");
+    
+}
+
+console.log(typeof);
