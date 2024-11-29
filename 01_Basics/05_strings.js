51896c568ac2f93da485b8c93fc3fcff757f0aa0
Khawaja Hassan Shahid <mrbeastvenom69@gmail.com>
2024-11-29 16:17:19 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "hassan"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('KhHassan')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherstring = gameName.slice(-8, 4)
+console.log(anotherstring);
+
+
+
+
