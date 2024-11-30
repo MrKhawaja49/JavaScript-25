31111f7aabec98f2e52a7cf519ce879277cd384d
Khawaja Hassan Shahid <mrbeastvenom69@gmail.com>
2024-11-30 09:18:18 +0000

    2_Arrays

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,11 @@
+const marvelHeros = ["thor", "ironman", "spiderman"]
+const dcHeros = ["superman", "flash", "batman"]
+
+marvelHeros.push(dcHeros)
+
+console.log(marvelHeros);
+console.log(marvelHeros[3][1]);
+
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
